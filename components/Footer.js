ffa610ab2377936ebf61628f631e22f66c9d9619
laithfayizhussein <laithalsanory9919@gmail.com>
2021-09-14 16:37:44 +0300

    finsh-lap-37

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer (){
+
+    return(
+        <footer className="p-4 bg-green-500 ">
+            <h1 className="flex text-leftext-black i text-x1" >&copy;2021</h1>
+        </footer>
+    )
+}
